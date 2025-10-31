8c29e5bdca44af7c4cd4c5414da956eb1e60e2bb
Asif Ahammed <asifahammed359@gmail.com>
2025-11-01 04:48:43 +0600

    fix deploy issue 6

--- src/admin/component-loader.ts
+++ src/admin/component-loader.ts
@@ -3,6 +3,6 @@ import path from 'path';
 
 const componentLoader = new ComponentLoader();
 // AdminJS bundles React components from source files, so always use src path
-componentLoader.override('Login', path.join(process.cwd(), 'src/admin/components/Login'));
-componentLoader.add('Dashboard', path.join(process.cwd(), 'src/admin/components/Dashboard'));
+componentLoader.override('Login', 'src/admin/components/Login');
+componentLoader.add('Dashboard', 'src/admin/components/Dashboard');
 export default componentLoader;
